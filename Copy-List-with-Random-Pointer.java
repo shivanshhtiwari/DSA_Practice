3fa3b148bc825aca3179707aa964701a4989a92f
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-07-21 02:36:13 +0530

    [138] [Time Beats: 100.00%] [Memory Beats: 92.91%] - LeetPush

---
+++ Copy-List-with-Random-Pointer.java
@@ -0,0 +1,49 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        Node curr = head;
+        while (curr != null) {
+            Node clone = new Node(curr.val);
+            clone.next = curr.next;
+            curr.next = clone;
+            curr = clone.next;
+        }
+        curr = head;
+        while (curr != null) {
+            if (curr.random != null) {
+                curr.next.random = curr.random.next;
+            }
+            curr = curr.next.next;
+        }
+
+        curr = head;
+        Node pseudoHead = new Node(0);
+        Node copy = pseudoHead;
+
+        while (curr != null) {
+            copy.next = curr.next;
+            curr.next = curr.next.next;
+
+            curr = curr.next;
+            copy = copy.next;
+        }
+
+        return pseudoHead.next;
+    }
+}
