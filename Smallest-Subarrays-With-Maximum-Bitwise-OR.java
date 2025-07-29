e4ef4d12adeee2bebe05c3b68132dc85e1ef5f2c
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-07-29 18:24:15 +0530

    [2411] [Time Beats: 28.38%] [Memory Beats: 44.59%] - LeetPush

---
+++ Smallest-Subarrays-With-Maximum-Bitwise-OR.java
@@ -0,0 +1,18 @@
+class Solution {
+  public int[] smallestSubarrays(int[] nums) {
+    final int MAX_BIT = 30;
+    int[] ans = new int[nums.length];
+    int[] closest = new int[MAX_BIT];
+
+    Arrays.fill(ans, 1);
+
+    for (int i = nums.length - 1; i >= 0; --i)
+      for (int j = 0; j < MAX_BIT; ++j) {
+        if ((nums[i] >> j & 1) == 1)
+          closest[j] = i;
+        ans[i] = Math.max(ans[i], closest[j] - i + 1);
+      }
+
+    return ans;
+  }
+}
