1d84a84b141f8eded632c75cf6fccaa8b54950f8
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-07-31 11:10:49 +0530

    [136] [Time Beats: 99.85%] [Memory Beats: 35.41%] - LeetPush

--- Single-Number.java
+++ Single-Number.java
@@ -1,17 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Map<Integer, Integer> hash= new HashMap<>();
-        for(int num:nums)
+        int result=0;
+        for(int num: nums)
         {
-            hash.put(num, hash.getOrDefault(num,0)+1);
+            result ^=num;
         }
-        for(Map.Entry<Integer, Integer> entry: hash.entrySet())
-        {
-            if(entry.getValue()==1)
-            {
-                return entry.getKey();
-            }
-        }
-        return -1;
+        return result;
     }
 }
