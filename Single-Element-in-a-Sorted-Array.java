65e8a4df4c9b269fc0a2affcb7f6f25deb3e370c
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-08-05 19:36:24 +0530

    [540] [Time Beats: 100.00%] [Memory Beats: 41.23%] - LeetPush

--- Single-Element-in-a-Sorted-Array.java
+++ Single-Element-in-a-Sorted-Array.java
@@ -1,28 +1,24 @@
 class Solution {
-    public int singleNonDuplicate(int[] nums) {
-        int n= nums.length;
-        if(n==1) return nums[0];
-        int start=0;
-        int end= n-1;
-        while(start<=end)
-        {
-            int mid= start+ (end-start)/2;
-            if (mid == 0 && nums[0]!=nums[1]) return nums[mid];
-            if (mid == n-1 && nums[n-1]!=nums[n-2]) return nums[mid];
+    public int singleNonDuplicate(int[] arr) {
+        int n = arr.length;
+        if (n == 1) return arr[0];
 
-            if(nums[mid]!=nums[mid-1] && nums[mid]!=nums[mid+1]) return nums[mid];
+        int start = 0;
+        int end = n - 1;
 
-            if(mid%2==0)
-            {
-                if(nums[mid]==nums[mid-1]) end= mid-1;
-                else start=mid+1;
-            }
-            else
-            {
-                if(nums[mid]==nums[mid-1]) start=mid+1;
-                else end=mid-1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+
+      
+            if (mid % 2 == 1) mid--;
+
+            if (arr[mid] == arr[mid + 1]) {
+                start = mid + 2;
+            } else {
+                end = mid;
             }
         }
-        return -1;
+
+        return arr[start];
     }
 }
