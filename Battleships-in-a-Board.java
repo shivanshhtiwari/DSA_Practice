a37a6fdbc2043498de3e948257ff6fcca8e1ea74
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-08-13 19:29:31 +0530

    [419] [Time Beats: 100.00%] [Memory Beats: 83.14%] - LeetPush

--- Battleships-in-a-Board.java
+++ Battleships-in-a-Board.java
@@ -1,19 +1,32 @@
-class Solution {
-    public int countBattleships(char[][] board) {
+class Solution 
+{
+    public int countBattleships(char[][] board) 
+    {
         int m = board.length;
         int n = board[0].length;
         int count = 0;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (board[i][j] == 'X') {
-                    if ((i == 0 || board[i - 1][j] != 'X') &&
-                        (j == 0 || board[i][j - 1] != 'X')) {
-                        count++;
-                    }
+        for (int i = 0; i < m; i++) 
+        {
+            for (int j = 0; j < n; j++) 
+            {
+                if (board[i][j] == 'X') 
+                {
+                    countbattle(board, i, j, m, n);
+                    count++;
                 }
             }
         }
         return count;
+    }
+    public void countbattle(char[][] board, int i, int j, int m, int n)
+    {
+        if(i<0 || j<0 || i>=m || j>=n || board[i][j]!='X') return;
+
+        board[i][j]='.';
+        countbattle(board, i+1,j,m,n);
+        countbattle(board, i-1,j,m,n);
+        countbattle(board, i,j+1,m,n);
+        countbattle(board, i,j-1,m,n);
     }
 }
