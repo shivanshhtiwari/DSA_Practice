b7d842672c5d7cbf4e5d4290c87087bce1866212
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-09-23 11:55:03 +0530

    [1] [Time Beats: 98.90%] [Memory Beats: 65.22%] - LeetPush

--- Two-Sum.java
+++ Two-Sum.java
@@ -1,15 +1,13 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Integer> Map= new HashMap<>();
-        for(int i=0;i<nums.length;i++)
-        {
-            int complement= target-nums[i];
-            if(Map.containsKey(complement))
-            {
-                return new int[] {Map.get(complement),i};
+         Map<Integer, Integer> numMap= new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] { numMap.get(complement), i };
             }
-            Map.put(nums[i], i);
+            numMap.put(nums[i], i);
         }
-        return new int[] {};
+        return new int[]{};
     }
 }
