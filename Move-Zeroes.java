d9d3270d483549c97cfb3ac6ca3b516e7e60363b
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-09-23 12:06:31 +0530

    [283] [Time Beats: 99.65%] [Memory Beats: 92.47%] - LeetPush

--- Move-Zeroes.java
+++ Move-Zeroes.java
@@ -1,7 +1,6 @@
 class Solution {
-    public void moveZeroes(int[] nums) {
-        int insertPos = 0;
-        
+    public int[] moveZeroes(int[] nums) {
+          int insertPos = 0;
         for (int num : nums) {
             if (num != 0) {
                 nums[insertPos++] = num;
@@ -10,5 +9,6 @@ class Solution {
         while (insertPos < nums.length) {
             nums[insertPos++] = 0;
         }
+        return nums;
     }
 }
