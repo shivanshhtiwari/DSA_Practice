93a549849f9ad1ab0b569d2eb8a3e74ee48de57f
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-09-29 14:56:01 +0530

    [151] [Time Beats: 85.18%] [Memory Beats: 40.74%] - LeetPush

--- Reverse-Words-in-a-String.java
+++ Reverse-Words-in-a-String.java
@@ -1,16 +1,12 @@
 class Solution {
     public String reverseWords(String s) {
-        String[] words = s.trim().split("\\s+");
-        StringBuilder result = new StringBuilder();
-
-        for (int i = words.length - 1; i >= 0; i--) {
-            result.append(words[i]);
-
-            if (i != 0) {
-                result.append(" ");
-            }
+        String[] words= s.split(" +");
+        StringBuilder sb= new StringBuilder();
+        for(int i=words.length-1; i>=0;i--)
+        {
+            sb.append(words[i]);
+            sb.append(" ");
         }
-
-        return result.toString();
+        return sb.toString().trim();
     }
 }
