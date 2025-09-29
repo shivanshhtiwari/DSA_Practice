5cd28a2310bc780710a5600c36cc7f4dcbb9de86
Shivansh Tiwari <113783549+shivanshrider@users.noreply.github.com>
2025-09-29 13:08:58 +0530

    [58] [Time Beats: 100.00%] [Memory Beats: 82.04%] - LeetPush

--- Length-of-Last-Word.java
+++ Length-of-Last-Word.java
@@ -1,16 +1,17 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int i= s.length()-1;
-        int length=0;
-        while(i>=0 && s.charAt(i)== ' ')
+        String str= s.trim();
+        int n=0;
+        for(int i=str.length()-1;i>=0;i--)
         {
-            i--;
+            if(str.charAt(i)!=' ')
+            {
+                n++;
+            }
+            else{
+                break;
+            }
         }
-        while(i>=0 && s.charAt(i)!= ' ')
-        {
-            length++;
-            i--;
-        }
-        return length;
+        return n;
     }
 }
